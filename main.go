99a6563e06c563889c23971d32bd743e720f4cb6
YunChuan1 <bravo102795@gmail.com>
2017-05-24 14:12:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -35,11 +35,11 @@ func main() {
 //MessageReceived :Callback to handle when message received.
 func MessageReceived(event Event, opts MessageOpts, msg ReceivedMessage) {
 	//log.Println("event:", event, " opt:", opts, " msg:", msg)
-	profile, err := mess.GetProfile(opts.Sender.ID)
+	/*profile, err := mess.GetProfile(opts.Sender.ID)
 	if err != nil {
 		fmt.Println(err)
 		return
-	}
+	}*/
 	resp, err := mess.SendSimpleMessage(opts.Sender.ID, fmt.Sprintf("Hello   , %s %s, %s", profile.FirstName, profile.LastName, msg.Text))
 	if err != nil {
 		fmt.Println(err)
